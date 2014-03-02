25fe5850a5b0e1acee7dfe99358d43f207b8b6a3
Fritz Obermeyer <fobermeyer@salesforce.com>
2014-03-01 19:13:21 -0800

    Simplify test_dd.py

--- distributions/tests/test_dd.py
+++ distributions/tests/test_dd.py
@@ -7,35 +7,19 @@ MODELS = [v.Model for v in VERSIONS]
 
 
 def test_methods_run():
-    alphas = [0.2, 0.5, 1.0, 2.0]
-    dim = len(alphas)
-    data = [3, 2, 3, 1, 3, 2, 3, 0, 3, 2, 3, 1]
+    EXAMPLE = MODELS[0].EXAMPLE
+    raw_model = EXAMPLE['model']
+    values = EXAMPLE['values']
 
-    raw_model = {'alphas': alphas}
-    models = []
-    for Model in MODELS:
-        model = Model()
-        model.load(raw_model)
-        Model.load_model(raw_model)
-        models.append(model)
+    models = [Model.load_model(raw_model) for Model in MODELS]
 
-    raw_group = {'counts': [0] * dim}
     for model in models:
-        group = model.Group()
-        group.load(raw_group)
-        group2 = model.load_group(raw_group)
+        group1 = model.Group()
+        group2 = model.Group()
+        model.group_init(group1)
+        model.group_init(group2)
 
-        model.group_init(group)
-        for value in data:
-            model.group_add_value(group, value)
-        model.sample_value(group)
-        model.score_group(group)
-        for value in data:
-            model.score_value(group, value)
-        model.group_merge(group2, group)
-        assert_equal(group2.dump(), group.dump())
-        for value in data:
-            model.group_remove_value(group, value)
-
-        model.dump_group(group)
-        model.dump()
+        for value in values:
+            model.group_add_value(group1, value)
+        model.group_merge(group2, group1)
+        assert_equal(group1.dump(), group2.dump())
