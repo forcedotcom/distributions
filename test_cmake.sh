64b0b82ea5158b41572e7e3f5a2d9351c606f4df
Fritz Obermeyer <fobermeyer@salesforce.com>
2014-03-05 19:19:58 +0000

    Update cmake test example

--- test_cmake.sh
+++ test_cmake.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 BUILD=examples/cmake/build
+ROOT=$PWD
 
 mkdir -p $BUILD
 cd $BUILD
-cmake ..
-make
-./foo
+
+echo '------------'
+echo 'REMOTE BUILD'
+cmake .. && make && ./foo
+
+echo '-----------'
+echo 'LOCAL BUILD'
+DISTRIBUTIONS_PATH=$ROOT cmake .. && make && ./foo
