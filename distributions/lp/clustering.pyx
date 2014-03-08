c94508d279471845bd9fbf76577f32c720b6eba9
Fritz Obermeyer <fobermeyer@salesforce.com>
2014-03-08 08:45:28 -0800

    Implement cython wrapper of Pitman-Yor model

--- distributions/lp/clustering.pyx
+++ distributions/lp/clustering.pyx
@@ -7,56 +7,40 @@ cdef extern from 'distributions/clustering.hpp':
 
     cppclass Assignments_cc "distributions::Clustering<int>::Assignments":
         Assignments_cc() nogil except +
-        Assignments_cc(Assignments_cc &) nogil except +
         cppclass iterator:
             pair[int, int] & operator*() nogil
             iterator operator++() nogil
-            bint operator==(iterator) nogil
             bint operator!=(iterator) nogil
         int & operator[](int) nogil
-        iterator begin() nogil
-        iterator end() nogil
-        void clear() nogil
-        bint empty() nogil
 
-    cdef vector[int] count_assignments \
+    cdef vector[int] count_assignments_cc \
             "distributions::Clustering<int>::count_assignments" \
             (Assignments_cc & assignments) nogil
 
     cppclass PitmanYor_cc "distributions::Clustering<int>::PitmanYor":
+        float alpha
+        float d
         vector[int] sample_assignments(int size, rng_t & rng) nogil
         float score_counts(vector[int] & counts) nogil
         float score_add_value (
-                int this_group_size,
-                int total_group_count,
-                int total_value_count) nogil
+                int group_size,
+                int group_count,
+                int sample_size) nogil
         float score_remove_value (
-                int this_group_size,
-                int total_group_count,
-                int total_value_count) nogil
+                int group_size,
+                int group_count,
+                int sample_size) nogil
 
 
-cdef class Assignments:
-    cdef Assignments_cc * ptr
-    def __cinit__(self):
-        self.ptr = new Assignments_cc()
-    def __dealloc__(self):
-        del self.ptr
-
-    def load(self, raw):
-        self.ptr.clear()
-        cdef int value_id
-        cdef int group_id
-        for value_id, group_id in raw.iteritems():
-            deref(self.ptr)[value_id] = group_id
-
-    def dump(self):
-        cdef dict raw = {}
-        cdef Assignments_cc.iterator i = self.ptr.begin()
-        while i != self.ptr.end():
-            deref(self.ptr)[deref(i).first] = deref(i).second
-            inc(i)
-        return raw
+cpdef list count_assignments(dict assignments):
+    cdef Assignments_cc assignments_cc
+    cdef int value_id
+    cdef int group_id
+    for value_id, group_id in assignments.iteritems():
+        assignments_cc[value_id] = group_id
+    cdef vector[int] counts_cc = count_assignments_cc(assignments_cc)
+    cdef list counts = counts_cc
+    return counts
 
 
 cdef class PitmanYor:
@@ -65,3 +49,41 @@ cdef class PitmanYor:
         self.ptr = new PitmanYor_cc()
     def __dealloc__(self):
         del self.ptr
+
+    def __init__(self, float alpha=0.5, float d=0.0):
+        self.ptr.alpha = alpha
+        self.ptr.d = d
+
+    property alpha:
+        def __get__(self):
+            return self.ptr.alpha
+        def __set__(self, float value):
+            self.ptr.alpha = value
+
+    property d:
+        def __get__(self):
+            return self.ptr.d
+        def __set__(self, float value):
+            self.ptr.d = value
+
+    def score_counts(self, list counts):
+        cdef vector[int] counts_cc = counts
+        cdef float score = self.ptr.score_counts(counts_cc)
+        return score
+
+    def score_add_value(
+            self,
+            int group_size,
+            int group_count,
+            int sample_size):
+        return self.ptr.score_add_value(group_size, group_count, sample_size)
+
+    def score_remove_value(
+            self,
+            int group_size,
+            int group_count,
+            int sample_size):
+        return self.ptr.score_remove_value(
+            group_size,
+            group_count,
+            sample_size)
