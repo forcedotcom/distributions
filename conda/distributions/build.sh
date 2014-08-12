b485c955e4324bc313c5768ea83c69a1dce42950
Stephen Tu <stephen@qadium.com>
2014-08-12 11:05:59 -0700

    some conda fixes

--- conda/distributions/build.sh
+++ conda/distributions/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+if [ "`uname`" = "Darwin" ]; then
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+fi
 echo "Conda build env"
 printenv
 echo "protoc: `which protoc`"
-# XXX(stephentu): replace with git tag once this is all in place
-SHA1=e1c80d04bb94a2af2f6384066a2c1209869a04ff
 git clone https://github.com/forcedotcom/distributions.git
-cd distributions && git checkout ${SHA1}
+cd distributions && git checkout tags/2.0.21
 make protobuf
 mkdir build && cd build
 DISTRIBUTIONS_USE_PROTOBUF=1 cmake \
